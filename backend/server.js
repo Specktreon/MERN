7787d409423049a83a9679795c196c72f63846c5
Specktreon <lezzar37@gmail.com>
2025-03-14 11:51:10 +0100

    get all the products

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,18 @@ dotenv.config();
 const app = express();
 app.use(express.json()); //allows us to accept json data in red.body
 
+// get all products
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+//add a product
 app.post("/api/products", (req, res) => {
   const product = req.body;
 
@@ -28,15 +40,16 @@ app.post("/api/products", (req, res) => {
   }
 });
 
-// console.log(process.env.MONGO_URI);
-
+//delete a product
 app.delete("/api/products/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Product deleted" });
-  } catch (error) {}
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Product not found" });
+  }
 });
 
 app.listen(5000, async () => {
