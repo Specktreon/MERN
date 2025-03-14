3ac0f2ca59590b9d755634694522fc4908d7359b
Specktreon <lezzar37@gmail.com>
2025-03-14 11:06:56 +0100

    delete method

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,11 @@ app.post("/api/products", (req, res) => {
 
 app.delete("/api/products/:id", async (req, res) => {
   const { id } = req.params;
-  console.log("id", id);
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {}
 });
 
 app.listen(5000, async () => {
