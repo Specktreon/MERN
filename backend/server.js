216a5b9a60ef67c7743f70b9f2632e3f6bf94a78
Specktreon <lezzar37@gmail.com>
2025-03-15 06:22:46 +0100

    get all products

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -37,6 +38,27 @@ app.post("/api/products", (req, res) => {
   } catch (error) {
     console.error({ "Error in Create product:": error.message });
     res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
   }
 });
 
