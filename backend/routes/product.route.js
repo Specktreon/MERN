81dd289b06cff212329178ce9b2cbd222b207016
Specktreon <lezzar37@gmail.com>
2025-03-15 11:29:35 +0100

    creating router

---
+++ backend/routes/product.route.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+import {
+  createProduct,
+  deleteProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/product.controller.js";
+
+const router = express.Router();
+
+// get all products
+router.get("/", getProducts);
+
+//add a product
+router.post("/", createProduct);
+
+//update a product
+router.put("/:id", updateProduct);
+
+//delete a product
+router.delete("/:id", deleteProduct);
+
+export default router;
