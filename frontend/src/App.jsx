f15fccd2c3fdf6ad0b4ba1300aed070042117df5
Specktreon <lezzar37@gmail.com>
2025-03-17 13:36:51 +0100

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Box minH={"100vh"}>
+      <Navbar />
       <Routes>
-        <Route path="/" eLement={<HomePage />} />
-        <Route path="/create" eLement={<CreatePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
       </Routes>
     </Box>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,20 @@
+import { Container, Flex } from "@chakra-ui/react";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      ></Flex>
+    </Container>
+  );
 };
 
 export default Navbar;
