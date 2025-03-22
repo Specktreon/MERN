24cad25252e624b0aad0bb8d1d1f9a84f13a6f72
Specktreon <lezzar37@gmail.com>
2025-03-22 12:39:38 +0100

    product display

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
-import { Container, VStack, Text } from "@chakra-ui/react";
-import React from "react";
+import { Container, VStack, Text, SimpleGrid } from "@chakra-ui/react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
+import ProductCard from "@/components/ProductCard";
 
 const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("products", products);
+
   return (
     <Container maxW="container.xl" py={12}>
       <VStack spacing={8}>
@@ -15,6 +23,12 @@ const HomePage = () => {
         >
           Current Products
         </Text>
+
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} w={"full"}>
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
 
         <Text
           fontSize="xl"
