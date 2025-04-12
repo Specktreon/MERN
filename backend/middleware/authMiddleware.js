9a85fcc316329d51db7638a09ba41446c2b2d0b9
Specktreon <lezzar37@gmail.com>
2025-04-12 23:26:40 +0200

    setting up middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,11 +1,9 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/user.model.js";
 
 const authMiddleware = async (req, res, next) => {
-  // Get token from header
   const token = req.header("Authorization");
 
-  // Check if token exists
   if (!token) {
     return res
       .status(401)
@@ -13,13 +11,11 @@ const authMiddleware = async (req, res, next) => {
   }
 
   try {
-    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.userId); // Fetch user from DB
+    const user = await User.findById(decoded.userId);
 
-    // Attach user ID to request
-    req.user = { userId: user._id, isAdmin: user.isAdmin }; // Attach userId + isAdmin
-    next(); // Move to next middleware or route handler
+    req.user = { userId: user._id }; // Attach only userId
+    next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
   }
